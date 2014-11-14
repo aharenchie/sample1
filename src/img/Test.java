c57f05429251378900e4c4f5f0944e295ee0ee17
aharenchie <e125731@ie.u-ryukyu.ac.jp>
2014-11-15 01:06:06 +0900

    anser MMDEx6

--- src/img/Test.java
+++ src/img/Test.java
@@ -19,8 +19,23 @@ public class Test {
         for(int y=0;y<h;y++){
             for(int x=0;x<w;x++){
                 int c1 = read1.getRGB(x, y);
+                int a1 = ImageUtility.a(c1);
+                int r1 = ImageUtility.r(c1);
+                int g1 = ImageUtility.g(c1);
+                int b1 = ImageUtility.b(c1);
+                
                 int c2 = read2.getRGB(x, y);
-                int new_c = (c1+c2)/2;
+                int a2 = ImageUtility.a(c2);
+                int r2 = ImageUtility.r(c2);
+                int g2 = ImageUtility.g(c2);
+                int b2 = ImageUtility.b(c2);
+                
+                int a = (a1 + a2)/2;
+                int r = (r1 + r2)/2;
+                int g = (g1 + g2)/2;
+                int b = (b1 + b2)/2;
+                
+                int new_c = ImageUtility.argb(a, r, g, b);
                 
                 write.setRGB(x,y,new_c);
                 
